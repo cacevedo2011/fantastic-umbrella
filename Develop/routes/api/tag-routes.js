f4309d62f520eb5c075775cabd61477f4b5db385
cacevedo2011 <cristianacevedo2011@gmail.com>
2020-11-01 03:27:55 -0500

    Big Update
    This has a lot of thing packed, since last git push I added a lot of coding on all of the models and added new node_modules inside

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,11 +6,40 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+          include: {
+              model: Product,
+              attributes: ['product_name', 'price', 'stock', 'category_id']
+          }
+      })
+      .then(dbTagData => res.json(dbTagData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
+  Tag.findOne({
+    include: {
+        model: Product,
+        attributes: ['product_name', 'price', 'stock', 'category_id']
+    }
+  })
+.then((dbTagData) => {
+  if (!dbTagData) {
+    res.status(404).json({ message: "No user found with this id" });
+    return;
+  }
+  res.json(dbTagData);
+  })
+.catch((err) => {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.post('/', (req, res) => {
